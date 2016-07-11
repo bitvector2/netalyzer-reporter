65e4ca0257c2dd1b75a3e82372bbb073fd6cd977
Steven Logue <bitvector2@gmail.com>
2016-07-11 16:48:50 -0700

    syncpoint

--- src/main/scala/com/microsoft/netalyzer/reporter/Main.scala
+++ src/main/scala/com/microsoft/netalyzer/reporter/Main.scala
@@ -22,4 +22,51 @@ object Main {
     println("Elapsed Time: " + (t1 - t0) / 1000)
   }
 
+
+  /*
+  def materializeDeltas(sc: SQLContext): Unit = {
+
+      sc.sql(
+      """
+        CREATE TABLE IF NOT EXISTS netalyzer.deltas (
+          deltaseconds INT,
+          deltarxbytes INT,
+          deltatxbytes INT,
+          id VARCHAR(127)
+        )
+        CLUSTERED BY(id) INTO 16 BUCKETS
+        STORED AS ORC
+        TBLPROPERTIES("transactional"="true")
+      """.stripMargin
+    )
+
+    val deltasDf = sc.sql(
+      """
+        SELECT unix_timestamp(datetime) - lag(unix_timestamp(datetime)) OVER (PARTITION BY hostname, portname ORDER BY datetime) AS deltaseconds,
+          CASE WHEN (lag(totalrxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime) > totalrxbytes)
+            THEN round(18446744073709551615 - lag(totalrxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime) + totalrxbytes)
+            ELSE round(totalrxbytes - lag(totalrxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime))
+          END AS deltarxbytes,
+          CASE WHEN (lag(totaltxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime) > totaltxbytes)
+            THEN round(18446744073709551615 - lag(totaltxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime) + totaltxbytes)
+            ELSE round(totaltxbytes - lag(totaltxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime))
+          END AS deltatxbytes,
+          id
+        FROM netalyzer.samples
+      """.stripMargin
+    ).repartition(16)
+
+    deltasDf.printSchema()
+    deltasDf.show(100)
+
+    sc.sql(
+      """
+        TRUNCATE TABLE netalyzer.deltas
+      """.stripMargin
+    )
+
+    deltasDf.write.mode("append").saveAsTable("netalyzer.deltas")
+  }
+  */
+
 }

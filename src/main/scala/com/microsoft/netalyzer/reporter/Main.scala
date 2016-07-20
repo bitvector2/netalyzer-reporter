f92a57e9003bba693584f09e6142d613559b33eb
Steven Logue <bitvector2@gmail.com>
2016-07-20 15:09:08 -0700

    syncpoint

--- src/main/scala/com/microsoft/netalyzer/reporter/Main.scala
+++ src/main/scala/com/microsoft/netalyzer/reporter/Main.scala
@@ -7,6 +7,8 @@ object Main {
 
   val settings = new Settings()
   val conf = new SparkConf()
+    .setMaster("local")
+    .setAppName("netalyzer-loader")
   val sc = new SparkContext(conf)
   val sqlContext = new HiveContext(sc)
 
@@ -15,29 +17,53 @@ object Main {
 
     val t0 = System.currentTimeMillis
 
-    val deltas = sqlContext.sql(
-      """
-        SELECT unix_timestamp(datetime) - lag(unix_timestamp(datetime)) OVER (PARTITION BY hostname, portname ORDER BY datetime) AS deltaseconds,
-          CASE WHEN (lag(totalrxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime) > totalrxbytes)
-            THEN round(18446744073709551615 - lag(totalrxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime) + totalrxbytes)
-            ELSE round(totalrxbytes - lag(totalrxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime))
-          END AS deltarxbytes,
-          CASE WHEN (lag(totaltxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime) > totaltxbytes)
-            THEN round(18446744073709551615 - lag(totaltxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime) + totaltxbytes)
-            ELSE round(totaltxbytes - lag(totaltxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime))
-          END AS deltatxbytes,
-          id
-        FROM netalyzer.samples
-      """.stripMargin
-    ).repartition(16)
+    /*
+        val deltas = sqlContext.sql(
+          """
+            SELECT unix_timestamp(datetime) - lag(unix_timestamp(datetime)) OVER (PARTITION BY hostname, portname ORDER BY datetime) AS deltaseconds,
+              CASE WHEN (lag(totalrxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime) > totalrxbytes)
+                THEN round(18446744073709551615 - lag(totalrxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime) + totalrxbytes)
+                ELSE round(totalrxbytes - lag(totalrxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime))
+              END AS deltarxbytes,
+              CASE WHEN (lag(totaltxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime) > totaltxbytes)
+                THEN round(18446744073709551615 - lag(totaltxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime) + totaltxbytes)
+                ELSE round(totaltxbytes - lag(totaltxbytes) OVER (PARTITION BY hostname, portname ORDER BY datetime))
+              END AS deltatxbytes,
+              id
+            FROM netalyzer.samples
+          """.stripMargin
+        ).repartition(16)
+
+        sqlContext.sql(
+          """
+            CREATE TABLE IF NOT EXISTS netalyzer.deltas (
+              deltaseconds INT,
+              deltarxbytes INT,
+              deltatxbytes INT,
+              id VARCHAR(127)
+            )
+            CLUSTERED BY(id) INTO 16 BUCKETS
+            STORED AS ORC
+            TBLPROPERTIES("transactional"="true")
+          """.stripMargin
+        )
+
+        sqlContext.sql(
+          """
+            TRUNCATE TABLE netalyzer.deltas
+          """.stripMargin
+        )
+
+        deltas.write.mode("append").saveAsTable("netalyzer.deltas")
+    */
 
     val t1 = System.currentTimeMillis
+
     println("Elapsed Time: " + (t1 - t0) / 1000)
 
-    val jdbcProp = new java.util.Properties
-    jdbcProp.setProperty("user", settings.jdbcUser)
-    jdbcProp.setProperty("password", settings.jdbcPassword)
-
-    deltas.write.mode("overwrite").jdbc(settings.jdbcUrl, "NETALYZER_DELTAS", jdbcProp)
+    // val jdbcProp = new java.util.Properties
+    // jdbcProp.setProperty("user", settings.jdbcUser)
+    // jdbcProp.setProperty("password", settings.jdbcPassword)
+    // deltas.write.mode("overwrite").jdbc(settings.jdbcUrl, "NETALYZER_DELTAS", jdbcProp)
   }
 }
